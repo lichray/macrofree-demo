594fe537570571354bdc4a94e834b030943a799d
Zhihao Yuan <lichray@gmail.com>
2019-09-25 03:08:20 -0500

    feat: port existing functions and setup build

---
+++ include/macrofree_demo/c_file_funcs.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <stdio.h>
+#include <system_error>
+
+namespace macrofree_demo
+{
+
+inline FILE* xfopen(char const* fn, char const* mode)
+{
+#if !defined(_WIN32)
+	auto fp = ::fopen(fn, mode);
+#else
+	FILE* fp;
+	fopen_s(&fp, fn, mode);
+#endif
+	if (fp == nullptr)
+		throw std::system_error(errno, std::system_category());
+
+	return fp;
+}
+
+struct c_file_deleter
+{
+	void operator()(FILE* fp) const { ::fclose(fp); }
+};
+
+}
